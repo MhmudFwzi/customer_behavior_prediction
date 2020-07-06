37b6c354c56add69e0f502d30e9a541ed1b2cf29
anaramirli <anar.amirli@gmail.com>
2020-07-06 19:19:13 +0200

    updates in documents.

--- modules/StatUtils.py
+++ modules/StatUtils.py
@@ -5,6 +5,8 @@ contains some of our stat tools
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from scipy import stats
+from statsmodels.stats.contingency_tables import mcnemar
 import matplotlib.pyplot as plt
 
 
@@ -52,4 +54,56 @@ class StatUtils(object):
         fig.axes[0].set_title(title)
         fig.axes[0].set_xlabel('Fitted values')
         fig.axes[0].set_ylabel('Residuals')
+        
+        
+        
+    def chi_squared_test(self, table):
+        '''
+        Prints the contingency table. The chi-squared test is calculated and 
+        the degrees of freedom is indicated. The critical value is calculated and
+        interpreted, finding that indeed the variables are independent or not.
+        
+        inputs:
+        
+            table: cross tab of given categorical (nominal) variables
+        '''
+        stat, p, df, expected = stats.chi2_contingency(table)
+
+        print('df=%d\n' % df)
+        
+        print('Expected values:\n')
+        expected = pd.DataFrame(expected)
+        expected.columns = table.columns
+        expected.index = table.index
+        print(expected)
+    
+        # interpret p-value
+        significance = 0.05
+        print('\nchi=%.4f, p-value=%.4f\n' % (stat, p))
+        
+        if p <= significance:
+            print('At significance=%.3f, reject null hypotheses: thus categorical variables are dependent' % significance)
+        else:
+            print('At significance=%.3f, accept null hypotheses: thus categorical variables are independent' % significance)
+            
+            
+    def mcnemar_test(self, table):
+      
+        '''
+        Caculates mcnemar test for categorical (nominal) varables, used when any frequency is small (<5) 
+        inputs:
+            table: cross tab of given categorical (nominal) variables
+        '''
+        
+        # calculate mcnemar test
+        result = mcnemar(table, exact=True)
+        # summarize the finding
+        print('statistic=%.4f, p-value=%.4f\n' % (result.statistic, result.pvalue))
+        # interpret the p-value
+        significance = 0.05
+        if result.pvalue <= significance:
+            print('At significance=%.4f, reject null hypotheses: categories has impact on each other' % significance)
+        else:
+            print('At significance=%.4f, accept null hypotheses: categories do not have impact on each other' % significance)
+
         
